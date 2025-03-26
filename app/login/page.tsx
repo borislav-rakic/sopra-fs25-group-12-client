e2fa873b10e09e674eb5d03a98b0dd21b4ad4924
stevaniaeilyn <stevania.frutiger@gmail.com>
2025-03-26 17:08:56 +0100

    Implement register page (#8) and login page (#9)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+"use client";
 
-import { useRouter } from "next/navigation"; // use NextJS router for navigation
+import { useRouter } from "next/navigation";
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
@@ -17,28 +17,22 @@ const Login: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
   const [form] = Form.useForm();
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
+  
   const {
-    // value: token, // is commented out because we do not need the token value
-    set: setToken, // we need this method to set the value of the token to the one we receive from the POST request to the backend server API
-    // clear: clearToken, // is commented out because we do not need to clear the token when logging in
-  } = useLocalStorage<string>("token", ""); // note that the key we are selecting is "token" and the default value we are setting is an empty string
-  // if you want to pick a different token, i.e "usertoken", the line above would look as follows: } = useLocalStorage<string>("usertoken", "");
+    set: setToken,
+  } = useLocalStorage<string>("token", "");
 
   const handleLogin = async (values: FormFieldProps) => {
     try {
-      // Call the API service and let it handle JSON serialization and error handling
-      const response = await apiService.post<User>("/users", values);
 
-      // Use the useLocalStorage hook that returned a setter function (setToken in line 41) to store the token if available
+      const response = await apiService.post<User>("/login", values);
+
       if (response.token) {
         setToken(response.token);
       }
 
-      // Navigate to the user overview
       router.push("/landingpageuser");
+
     } catch (error) {
       if (error instanceof Error) {
         alert(`Something went wrong during the login:\n${error.message}`);
@@ -60,7 +54,7 @@ const Login: React.FC = () => {
       >
         <Form.Item
           name="username"
-          label="Username"
+          label="Username:"
           rules={[{ required: true, message: "Please input your username!" }]}
         >
           <Input placeholder="Enter username" />
@@ -68,7 +62,7 @@ const Login: React.FC = () => {
 
         <Form.Item
           name="password"
-          label="Password"
+          label="Password:"
           rules={[{ required: true, message: "Please input your password!" }]}
         >
           <Input placeholder="Enter Password" />
@@ -77,7 +71,7 @@ const Login: React.FC = () => {
         <div style={{ display: "flex", justifyContent: "space-between", gap: "10px" }}>
           <Button
             className="back-button"
-            onClick={() => router.back()}
+            onClick={() => router.push("/")}
           >
             Back
           </Button>
