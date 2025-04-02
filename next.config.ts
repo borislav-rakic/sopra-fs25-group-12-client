f5a510596c7193387c5991c299bf295781ce8bcc
Dieter Studer-Joho <dieter@studer-joho.ch>
2025-04-02 12:53:30 +0200

    Update deprecated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['deckofcardsapi.com'],
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'deckofcardsapi.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
