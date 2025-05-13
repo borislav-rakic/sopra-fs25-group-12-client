8f53f6643cacc5a6fca87e88741f0c9c7b71695c
Dieter Studer-Joho <dieter@studer-joho.ch>
2025-05-13 17:29:35 +0200

    Adjust var names to match the backend.

--- app/types/user.ts
+++ app/types/user.ts
@@ -7,12 +7,12 @@ export interface User {
   status: string | null;
   scoreTotal: number;
   gamesPlayed: number;
-  avgPlacement: number;
+  avgGamePlacement: number;
   moonShots: number;
   perfectGames: number;
   perfectMatches: number;
-  currentStreak: number;
-  longestStreak: number;
+  currentGameStreak: number;
+  longestGameStreak: number;
 }
 
 export interface UserAuthDTO {
