a806669594e47ab6b6e68263e3d15620ed5dce7b
dominiqueheller <dominiqueheller@yahoo.com>
2025-05-15 01:53:48 +0200

    Added message-box for indicator of whose turn it is and who won the trick

--- app/types/polling.ts
+++ app/types/polling.ts
@@ -21,6 +21,7 @@ export interface PollingDTO {
   currentTrickLeaderMatchPlayerSlot: number | null;
   currentTrickLeaderPlayerSlot: number | null;
   currentTrickDTO: TrickDTO | null; // TrickDTO object
+  currentPlayerSlot: number | null;
 
   previousTrick: Array<innerCard> | null;
   previousTrickWinnerMatchPlayerSlot: number | null;
