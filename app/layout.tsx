bbd75539a39215a4d94500f63fc402eff5c23dae
Dieter Studer-Joho <dieter@studer-joho.ch>
2025-05-15 08:41:40 +0200

    Fixes for join functionality. #23

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   const pathname = usePathname();
   // Define pages where InviteHandler should not be rendered
   const excludeInviteHandler = ["/start", "/match"];
-  const shouldRenderInviteHandler = excludeInviteHandler.includes(pathname);
+  const shouldRenderInviteHandler = !excludeInviteHandler.includes(pathname);
 
   return (
     <html lang="en" suppressHydrationWarning>
